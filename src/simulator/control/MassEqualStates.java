59c73eab6df097b59504a8eab1837fc04015d822
Jan Majchrzycki <majchrzycki2000@gmail.com>
2021-03-11 15:42:38 +0100

    new classes

---
+++ src/simulator/control/MassEqualStates.java
@@ -0,0 +1,18 @@
+//package simulator.control;
+//
+//import org.json.JSONObject;
+//
+//public class MassEqualStates implements StateComparator{
+//
+//	@Override
+//	public boolean equal(JSONObject s1, JSONObject s2) {
+//		
+//		if(s1.get("time")==s2.get("time")) {
+//			
+//		}
+//		
+//		return false;
+//	}
+//	
+//
+//}
---
+++ src/simulator/factories/Builder.java
@@ -0,0 +1,18 @@
+package simulator.factories;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class Builder<T> implements Factory<T> {
+
+	public T createInstance(JSONObject info) {
+		
+		return null;
+	}
+
+	public List<JSONObject> getInfo() {
+		return null;
+	}
+
+}
